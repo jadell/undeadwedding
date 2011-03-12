bbe3758d0f3a553fa18c0d30d59968304a5c5ecf
Josh Adell <josh.adell@gmail.com>
2011-03-12 17:58:00 -0500

    Added voting for "application" demo.

--- html/28-days-later/content.php
+++ html/28-days-later/content.php
@@ -1,1 +1,35 @@
-28 Days Later
+<?php
+$dataFilePath = '../data.txt';
+
+if (!$loggedIn) {
+	header("Location: /login");
+} else if (isset($_POST['vote'])) {
+	file_put_contents($dataFilePath, $_POST['vote']."\n", FILE_APPEND|LOCK_EX);
+	header("Location: /28-days-later");
+}
+
+$dataFile = file($dataFilePath);
+$preferredWeapon = array();
+foreach ($dataFile as $choice) {
+	if (!isset($preferredWeapon[$choice])) {
+		$preferredWeapon[$choice] = 0;
+	}
+	$preferredWeapon[$choice]++;
+}
+
+arsort($preferredWeapon);
+?>
+
+<h2>Preferred Zombie Dispatching Implement</h2>
+<ul id="weapon">
+	<?php foreach ($preferredWeapon as $weapon => $count) : ?>
+		<li><?php echo $weapon; ?>: <?php echo $count; ?></li>
+	<?php endforeach; ?>
+</ul>
+
+<form id="voteForm" method="POST" action="">
+	<label for="vote">Your choice:</label>
+	<input id="vote" name="vote" type="text" value="" /><br />
+
+	<button type="submit">Vote</button>
+</form>
