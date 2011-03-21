4320cedeb6f8a6fae1788f933fa4a6821f81dbce
Norman Santos <santnorm@gmail.com>
2011-03-21 14:03:37 -0400

    Adding built with.

--- html/content.php
+++ html/content.php
@@ -4,4 +4,10 @@
 	<div style="text-align: center;">
 		<img src="/img/shaun.png" />
 	</div>
+	<h3>Built Using:</h3>
+	<ul>
+		<li><a href="http://nodejs.org/">node.js</a></li>
+		<li><a href="http://vowsjs.org/">Vows</a></li>
+		<li><a href="http://zombie.labnotes.org/">Zombie.js</a></li>
+	</ul>
 </div>
