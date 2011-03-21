75348796e274a4de651ef62a3f79c80e4dc10600
Norman Santos <santnorm@gmail.com>
2011-03-21 13:01:46 -0400

    Adding intro page.

--- html/content.php
+++ html/content.php
@@ -1,1 +1,7 @@
-test test test test
+<div>
+	<h3>Full stack automated testing you say?</h3>
+	<p>using <i>zombies</i> you say?</p>
+	<div style="text-align: center;">
+		<img src="/img/shaun.png" />
+	</div>
+</div>
